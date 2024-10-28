c99cd644a617774651174cfc4fff4aed3d5fa9fd
Darius Marc Cruz <cruzdariusmark@gmail.com>
2024-10-28 12:09:12 +0800

    hamburger added/ javascript added

---
+++ script.js
@@ -0,0 +1,64 @@
+// for hamburger (header)
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".sidemenu");
+const navLinks = document.querySelectorAll(".sidemenu li");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    });
+});
+
+
+// scroll up/down (header)
+var lastScrollTop = 0;
+navBar = document.getElementById("navbar");
+window .addEventListener("scroll", function(){
+var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+if(scrollTop > lastScrollTop){
+    navBar.style.top = "-70px";
+}
+else{
+    navBar.style.top = "0";
+}
+lastScrollTop = scrollTop;
+})
+
+// MISSION VISION AND QUALITY POLICY
+var tablinks = document.getElementsByClassName("tab-links");    
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// LAZY LOADING
+document.addEventListener('DOMContentLoaded', function() {
+    const elements = document.querySelectorAll('.animate');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
